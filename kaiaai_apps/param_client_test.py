3564ab9c9e19b2d5bd89afd6f2ba2eff2cef4a60
Ilia O <iliao@kaia.ai>
2024-10-03 09:25:30 +0000

    ParamClient test

--- kaiaai_apps/param_client_test.py
+++ kaiaai_apps/param_client_test.py
@@ -1,15 +1,23 @@
+import rclpy
 from kaiaai.util import ParamClient
 
 
 def main():
   rclpy.init()
 
-  param_client = ParamClientAsync('/kaiaai_telemetry')
+  param_client = ParamClient('/kaiaai_telemetry_node')
+  param_name = 'battery.voltage_empty'
 
-  response = param_client.get(['my_parameter', 'your_parameter'])
-  param_client.get_logger().info(
-    'First value: %s, Second value: %s' %
-    (response.values[0].string_value, response.values[1].string_value))
+  response = param_client.get(param_name)
+#  param_client.get_logger().info('Value: %s' % (response.values[0].string_value))
+  value = param_client.to_value(response)
+  print(value)
+  value = value[0]
+
+  param_client.set(param_name, value + 0.1)
+
+  response = param_client.get(param_name)
+  print(param_client.to_value(response))
 
   param_client.destroy_node()
   rclpy.shutdown()
